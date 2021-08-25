299ccff6ee374033eee4e45f743b55470215f251
luyang <flowingsun007@163.com>
2021-08-25 16:35:29 +0800

    update script

--- resnet50/graph/train_consistent_fp16.sh
+++ resnet50/graph/train_consistent_fp16.sh
@@ -1,6 +1,8 @@
 # set -aux
+# export PYTHONPATH=/home/luyang/Oneflow/oneflow/python:$PYTHONPATH
+# nohup bash graph/train_consistent_fp16.sh > resnet50_fp16_graph_1.log 2>&1 &
 
-DEVICE_NUM_PER_NODE=1
+DEVICE_NUM_PER_NODE=8
 MASTER_ADDR=127.0.0.1
 NUM_NODES=1
 NODE_RANK=0
@@ -11,30 +13,19 @@ export NCCL_LAUNCH_MODE=PARALLEL
 echo NCCL_LAUNCH_MODE=$NCCL_LAUNCH_MODE
 export NCCL_DEBUG=INFO 
 
-OFRECORD_PATH="/DATA/disk1/ImageNet/ofrecord/"
-# if [ ! -d "$OFRECORD_PATH" ]; then
-#     wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/datasets/imagenette_ofrecord.tar.gz
-#     tar zxf imagenette_ofrecord.tar.gz
-# fi
+OFRECORD_PATH="/usr/local/datasets/imagenet/ofrecord/"
 
 CHECKPOINT_PATH="graph_amp_checkpoints"
 if [ ! -d "$CHECKPOINT_PATH" ]; then
     mkdir $CHECKPOINT_PATH
 fi
 
-PRETRAIN_MODEL_PATH="resnet50_imagenet_pretrain_model"
-if [ ! -d "$PRETRAIN_MODEL_PATH" ]; then
-  wget https://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/cv/classification/resnet50_imagenet_pretrain_model.tar.gz
-  tar zxf resnet50_imagenet_pretrain_model.tar.gz
-fi
-LOAD_CHECKPOINT="/DATA/disk1/ldp/OneFlow-Benchmark/Classification/cnns/output/snapshots/model_save-20210824083821/snapshot_epoch_0_graph"
-
 
 OFRECORD_PART_NUM=256
 LEARNING_RATE=1.536
 # LEARNING_RATE=1.024
 MOM=0.875
-EPOCH=50
+EPOCH=90
 TRAIN_BATCH_SIZE_PER_DEVICE=192
 # TRAIN_BATCH_SIZE_PER_DEVICE=128
 VAL_BATCH_SIZE_PER_DEVICE=50
@@ -58,6 +49,4 @@ python3 -m oneflow.distributed.launch \
     --warmup_epochs 5 \
     --use_fp16 \
     --nccl_fusion_threshold_mb=16 \
-    --nccl_fusion_max_ops=24 \
-    --load_checkpoint $LOAD_CHECKPOINT
-
+    --nccl_fusion_max_ops=24
